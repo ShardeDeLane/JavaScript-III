c694359d7c5e831724bd58a9f3dcc28c4891a7b2
ShardeDeLane <rhcprocker@yahoo.com>
2018-08-21 21:45:22 -0400

    prototypes and this

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,20 +1,26 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
 
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
+
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
-  
 /*
   === GameObject ===
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+function GameObject (gameobject) {
+  this.createdAt = gameobject.createdAt
+  this.dimensions = gameobject.dimensions
+}
+GameObject.prototype.destroy = function () {
+    return(console.log(`${this.name} was removed from the game.`))
 
+}
 /*
   === CharacterStats ===
   * hp
@@ -22,6 +28,18 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats (character) {
+  GameObject.call(this, character);
+  this.hp = character.hp
+  this.name = character.name
+}
+CharacterStats.prototype =
+Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return (console.log(`${this.name} took damage.`))
+}
+
 
 /*
   === Humanoid ===
@@ -32,7 +50,18 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+function Humanoid (human) {
+  CharacterStats.call(this, human)
+  this.faction = human.faction
+  this.weapons = human.weapons
+  this.language = human.language
+}
+Humanoid.prototype =
+Object.create(CharacterStats.prototype)
+
+Humanoid.prototype.greet = function () {
+  return(console.log(`${this.name} says hello in ${this.language}.`))
+}
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +70,7 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +131,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+
+  // Stretch task:
+  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - is the default setting when you have a function that is not pointing to anything such as an object, it will default to Window
+
+* 2. Implicit Binding - when dot notation is used to invoke a function, whatever is to the left of the dot becomes 'this'
+
+* 3. New Binding - when a constructor function is used 'this' refers to the instance of the object created by the constructor
+
+* 4. Explicit Binding - when .call(), .apply(), or .bind() is used on a function
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +15,51 @@
 // Principle 1
 
 // code example for Window Binding
+ function WindowGlobal = function () {
+     console.log(this)
+ }
+ console.log(windowGlobal())
 
 // Principle 2
 
 // code example for Implicit Binding
+var anObject = function(){
+    this.hero = hero
+    this.faction = faction
+}
+anObject.prototype.greeting = function(){
+    return(console.log(`${this.hero} says hello`))
+}
+
 
 // Principle 3
 
 // code example for New Binding
+var AnObject = function(){
+    this.hero = hero
+    this.faction = faction
+}
+anObject.prototype.greeting = function(){
+    return(console.log(`${this.hero} says hello`))
+}
+
+var ConstructorObject = new AnObject()
+ConstructorObject.greeting()
 
 // Principle 4
 
 // code example for Explicit Binding
+var jogger = {
+    name: "John",
+    activity: "running"
+}
+
+var callJogger = function(){
+    this.name = name
+    this.property = property
+}
+callJogger.prototype = function(activity){
+    console.log(`${this.name} is ${activity}`)
+}
+
+console.log(callJogger.call(jogger, jogger.activity))
